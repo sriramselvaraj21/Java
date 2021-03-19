fe291b0a2926602adca40283904962f9293a3dde
Sriram Selvaraj <Sriram.Selvaraj@gds.ey.com>
2021-03-19 08:38:50 +0530

    day 23

---
+++ day23/FrameDemo.java
@@ -0,0 +1,12 @@
+package day23;
+
+import java.awt.Frame;
+
+public class FrameDemo {
+	public static void main(String[] args) {
+		Frame frame = new Frame();
+		frame.setSize(400, 500);
+		frame.setVisible(true);
+		System.out.println("hi");
+	}
+}
