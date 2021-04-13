fb7cc5f45856601ba8d0a855a2466808c7ba95b9
Sriram Selvaraj <Sriram.Selvaraj@gds.ey.com>
2021-04-13 11:12:22 +0530

    hibernate learning

---
+++ hiber/Comment.java
@@ -0,0 +1,24 @@
+package hiber;
+
+import java.io.Serializable;
+
+public class Comment implements Serializable{
+	private String msg;
+	
+	public Comment() {
+		// TODO Auto-generated constructor stub
+	}
+	public Comment(String msg) {
+		this.msg=msg;
+	}
+	
+
+	public final String getMsg() {
+		return msg;
+	}
+
+	public final void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+}
